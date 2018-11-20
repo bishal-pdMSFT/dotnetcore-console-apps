2e9d968e31bc9461d607cf0d117b4a3a7fb613b6
Bishal Prasad <biprasad@microsoft.com>
2018-11-20 18:45:39 +0530

    read-words project with multiple target frameworks

---
+++ read-words/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace read_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
