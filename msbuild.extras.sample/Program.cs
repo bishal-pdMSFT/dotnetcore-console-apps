d39904f27fe6162a305fc4f91efb7b73093299e0
Bishal Prasad <biprasad@microsoft.com>
2019-10-30 19:57:21 +0530

    more projects

---
+++ msbuild.extras.sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace msbuild.extras.sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
